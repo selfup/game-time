e885040a8ff0d5738be2df72bb88c58243a1cc6e
selfup <boudinot.regis@yahoo.com>
2015-12-08 13:28:06 -0700

    awesome

--- test/first-test.js
+++ test/first-test.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
+const Tron = require('../lib/tron');
 
 describe('my test suite', function () {
   it('should work', function () {
     assert(true);
   });
 });
-// 
