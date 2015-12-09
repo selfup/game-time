be0b97b108a25e83f828c537d9f6fa8d7b86d1d4
selfup <boudinot.regis@yahoo.com>
2015-12-09 09:08:03 -0700

    more testing, better pushing into arrays

--- test/game-test.js
+++ test/game-test.js
@@ -6,10 +6,33 @@ const Grid = require('../lib/grid')
 
 describe('if the game works', function () {
 
-  it('the grid is intantiated properly', function () {
+  it('the game is intantiated properly', function () {
     let game = new Game
     assert.isObject(game)
+  })
+
+  it('the firstBikeTrail is intantiated properly', function () {
+    let game = new Game
     assert.deepEqual(game.grid.firstBikeTrail, [0, 300])
+  })
+
+  it('the secondBikeTrail is intantiated properly', function () {
+    let game = new Game
+    assert.deepEqual(game.grid.secondBikeTrail, [900, 300])
+  })
+
+  it('the combinedTrails is intantiated properly', function () {
+    let game = new Game
+    assert.deepEqual(game.grid.combinedTrails, [ 0, 300, 900, 300 ])
+  })
+
+  it('can add to combinedTrails on move', function () {
+    let game = new Game
+    game.bikeOne.moveRight
+    game.bikeTwo.moveLeft
+    game.fillBikeTrailOne
+    game.fillBikeTrailTwo
+    assert.deepEqual(game.grid.combinedTrails, [ 0, 300, 1, 300, 900, 300, 899, 300 ])
   })
 
 })
