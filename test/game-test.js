e321c850f0c59f7621fdbb61767b586914ddf904
zRouth <zrouthier@gmail.com>
2015-12-09 17:27:07 -0700

    add game status to declare if a player has run into another trail or died and thus the other player wins

--- test/game-test.js
+++ test/game-test.js
@@ -35,4 +35,30 @@ describe('if the game works', function () {
     assert.deepEqual(game.grid.combinedTrails, [ 0, 300, 1, 300, 900, 300, 899, 300 ])
   })
 
+  it('can declare a player has hit a trail and the game is over', function () {
+    let game = new Game
+    assert.deepEqual(game.statusArray, ["bikeOne alive", "bikeTwo alive"])
+  })
+
+  it('can declare a player is not dead after a move on start', function () {
+    let game = new Game
+    game.bikeOne.moveRight
+    game.bikeTwo.moveLeft
+    game.fillBikeTrailOne
+    game.fillBikeTrailTwo
+    assert.deepEqual(game.statusArray, ["bikeOne alive", "bikeTwo alive", "bikeOne alive", "bikeTwo alive"])
+  })
+
+  it('can declare a player is dead after a move into a trail', function () {
+    let game = new Game
+    game.statusArray.push("bikeTwo dead", "bikeOne alive")
+    assert.strictEqual(game.gameStatus, "Player One Wins!")
+  })
+
+  it('can declare that when both players hit each other its a double kill', function () {
+    let game = new Game
+    game.statusArray.push("bikeOne dead", "bikeTwo dead")
+    assert.strictEqual(game.gameStatus, "DOUBLE KILL!")
+  })
+
 })
