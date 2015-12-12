9a7626bb1ab1fc441f824b5f114b4c05348b8303
selfup <boudinot.regis@yahoo.com>
2015-12-12 13:17:48 -0700

    more test, all passing

--- test/game-test.js
+++ test/game-test.js
@@ -79,4 +79,75 @@ describe('if the game works', function () {
     assert.notStrictEqual(game.gameStatus, "DOUBLE KILL!")
   })
 
+  it('can declare a player has hit a trail and the game is over', function () {
+    let game = new Game
+    assert.deepEqual(game.statusArray, ["bikeOne alive", "bikeTwo alive"])
+  })
+
+  it('can declare a player is not dead after a move on start', function () {
+    let game = new Game
+    game.bikeOne.moveRight
+    game.bikeTwo.moveLeft
+    game.fillBikeTrailOne
+    game.fillBikeTrailTwo
+    assert.deepEqual(game.statusArray, ["bikeOne alive", "bikeTwo alive"])
+  })
+
+  it('can declare a player is dead after a move into a trail', function () {
+    let game = new Game
+    game.statusArray.push("bikeTwo Dead", "bikeOne alive")
+    assert.strictEqual(game.gameStatus, "Player One Wins!")
+  })
+
+  it('can declare that when both players hit each other its a double kill', function () {
+    let game = new Game
+    game.statusArray.push("bikeOne Dead", "bikeTwo Dead")
+    assert.strictEqual(game.gameStatus, "DOUBLE KILL!")
+  })
+
+   it('declares that both bikes are dead when it they colide', function () {
+    let game = new Game
+    for (let i = 0; i < 451; i++) {
+      game.bikeOne.moveRight
+      game.fillBikeTrailOne
+      game.bikeTwo.moveLeft
+      game.fillBikeTrailTwo
+    }
+    assert.strictEqual(game.gameStatus, "DOUBLE KILL!")
+  })
+
+  it('declares that a bike is not dead when it move a bunch but does not hit', function () {
+    let game = new Game
+    for (let i = 1; i < 450; i++) {
+      game.bikeOne.moveRight
+      game.fillBikeTrailOne
+      game.bikeTwo.moveLeft
+      game.fillBikeTrailTwo
+    }
+    assert.notStrictEqual(game.gameStatus, "DOUBLE KILL!")
+  })
+
+  it('declares that both bikes are alive when they move only once', function () {
+    let game = new Game
+    game.bikeOne.moveRight
+    game.fillBikeTrailOne
+    game.bikeTwo.moveLeft
+    game.fillBikeTrailTwo
+    assert.strictEqual(game.gameStatus, "alive")
+  })
+
+  it('declares that a bike is not dead when it move a bunch blah blah but does not hit', function () {
+    let game = new Game
+    for (let i = 1; i < 100; i++) {
+      game.bikeOne.moveRight
+      game.fillBikeTrailOne
+    }
+
+    for (let i = 1; i < 601; i++) {
+      game.bikeTwo.moveLeft
+      game.fillBikeTrailTwo
+    }
+    assert.notStrictEqual(game.gameStatus, "DOUBLE KILL!")
+  })
+
 })
